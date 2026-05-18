a818d3fc5d00eb8065728b8264205595b5df39b9
dudaa28 <164348330+dudaa28@users.noreply.github.com>
2026-05-17 21:57:20 -0300

    Feat:. Implementação da lógica e dashboard dos indicadores de desempenho (#282)
    * change main.py
    
    * fix error in main.py
    
    * minor changes on files
    
    * implementa script básico de websocket no frontend
    
    * fix error on parsing data from json
    
    * feat: implementacao da logica e dashboard dos indicadores de desempenho
    
    Co-authored-by: Euller <euller2005@gmail.com>
    
    ---------
    
    Co-authored-by: Gabriel <castelogabriel@outlook.com>
    Co-authored-by: Euller <euller2005@gmail.com>
    Co-authored-by: Euller Júlio <125329742+Potatoyz908@users.noreply.github.com>

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
-import MazeViewer from "./components/maze/MazeViewer";
+import { TelemetriaPage } from './pages/TelemetriaPage';
+import MazeViewer from './components/maze/MazeViewer';
 
 function App() {
   return (
     <main className="app">
+      <TelemetriaPage />
       <MazeViewer />
     </main>
   );
-=======
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
->>>>>>> 1d319fa4fef9c10a1841e14405ce03c5ff11e18b
 }
 
 export default App;
