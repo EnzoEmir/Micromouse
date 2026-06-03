9e9fb1d1a7a40b6bd094d1b8f8543b43f32df3cd
dudaa28 <eduardaq2805@gmail.com>
2026-06-03 01:16:05 -0300

    feat(us-17): cria SessionsPage com tabela de corridas e CardMelhorTempo

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -2,48 +2,41 @@ import { useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { LabirintoPage } from './pages/LabirintoPage';
 import { TelemetriaPage } from './pages/TelemetriaPage';
-import { CorridasPage } from './pages/CorridaPage';
+import { SessionsPage } from './pages/SessionsPage';
 import Session from './components/Session';
 
 function App() {
   const [currentView, setCurrentView] = useState<'session' | 'telemetria' | 'labirinto' | 'corridas'>('session');
 
   return (
-  <main className="app">
-    <Toaster position="top-right" />
-
-    {currentView === 'session' && (
-      <Session onNavigate={() => setCurrentView('telemetria')} />
-    )}
-
-    {currentView === 'telemetria' && (
-      <TelemetriaPage
-        activeView={currentView}
-        onNavigateTelemetria={() => setCurrentView('telemetria')}
-        onNavigateLabirinto={() => setCurrentView('labirinto')}
-        onNavigateCorridas={() => setCurrentView('corridas')}
-      />
-    )}
-
-    {currentView === 'labirinto' && (
-      <LabirintoPage
-        activeView={currentView}
-        onNavigateTelemetria={() => setCurrentView('telemetria')}
-        onNavigateLabirinto={() => setCurrentView('labirinto')}
-        onNavigateCorridas={() => setCurrentView('corridas')}
-      />
-    )}
-
-    {currentView === 'corridas' && (
-      <CorridasPage
-        activeView={currentView}
-        onNavigateTelemetria={() => setCurrentView('telemetria')}
-        onNavigateLabirinto={() => setCurrentView('labirinto')}
-        onNavigateCorridas={() => setCurrentView('corridas')}
-      />
-    )}
-  </main>
-);
+    <main className="app">
+      <Toaster position="top-right" />
+      {currentView === 'session' ? (
+        <Session onNavigate={() => setCurrentView('telemetria')} />
+      ) : currentView === 'telemetria' ? (
+        <TelemetriaPage
+          activeView={currentView}
+          onNavigateTelemetria={() => setCurrentView('telemetria')}
+          onNavigateLabirinto={() => setCurrentView('labirinto')}
+          onNavigateCorridas={() => setCurrentView('corridas')}
+        />
+      ) : currentView === 'labirinto' ? (
+        <LabirintoPage
+          activeView={currentView}
+          onNavigateTelemetria={() => setCurrentView('telemetria')}
+          onNavigateLabirinto={() => setCurrentView('labirinto')}
+          onNavigateCorridas={() => setCurrentView('corridas')}
+        />
+      ) : (
+        <SessionsPage
+          activeView={currentView}
+          onNavigateTelemetria={() => setCurrentView('telemetria')}
+          onNavigateLabirinto={() => setCurrentView('labirinto')}
+          onNavigateCorridas={() => setCurrentView('corridas')}
+        />
+      )}
+    </main>
+  );
 }
 
 export default App;
